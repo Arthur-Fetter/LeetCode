0c5181b69506d4e87a226fd2ea01f0d35b29f178
Arthur-Fetter <109872071+Arthur-Fetter@users.noreply.github.com>
2023-06-19 22:08:37 -0300

    876. Middle of the Linked List
    Final answer for challenge 876. Middle of the Linked List, working.
    Beats 21.62% in runtime.
    Beats 71.54% in memory.

---
+++ 876. Middle of the Linked List/Program.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        // Get node count
+        ListNode currentNode = head;
+        int node_count = 0;
+
+        while (currentNode != null)
+        {
+            node_count++;
+            currentNode = currentNode.next;
+        }
+
+        int middle_index = (node_count / 2);
+
+        currentNode = head;
+        ListNode finalList = null;
+
+        node_count = 0;
+        while (currentNode != null)
+        {
+            if (node_count >= middle_index)
+            {
+                finalList = currentNode;
+                break;
+            }
+
+            currentNode = currentNode.next;
+            node_count++;
+        }
+
+        return finalList;
+    }
+}
