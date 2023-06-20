cc141c59a5141f6d9f99c40a3beb7bd9bfb27e45
Arthur-Fetter <109872071+Arthur-Fetter@users.noreply.github.com>
2023-06-19 22:05:11 -0300

    1567. Maximum Length of Subarray With Positive Product
    Final answer for challange 1567. Maximum Length of Subarray With Positive Product, working. 
    Beats 73,68% in runtime.
    Beats 94.74% in memory.

---
+++ 1567. Maximum Length of Subarray With Positive Product/Program.cs
@@ -0,0 +1,60 @@
+﻿public class Solution
+{
+    public int GetMaxLen(int[] nums)
+    {
+        int max_count = 0;
+        int count = 0;
+        int count_negative = 0;
+
+        List<int> negative_occourences = new List<int>();
+        int starting_index = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                if (count_negative % 2 == 0)
+                {
+                    max_count = Math.Max(max_count, count);
+                }
+                else
+                {
+                    count = Math.Max((count - (negative_occourences[0] + 1 - starting_index)),
+                    (negative_occourences[negative_occourences.Count - 1] - starting_index));
+
+                    max_count = Math.Max(count, max_count);
+                }
+
+                negative_occourences.Clear();
+                count = 0;
+                count_negative = 0;
+                starting_index = i + 1;
+            }
+            else if (nums[i] < 0)
+            {
+                count++;
+                count_negative++;
+
+                negative_occourences.Add(i);
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        if (count_negative % 2 == 0)
+        {
+            max_count = Math.Max(max_count, count);
+        }
+        else
+        {
+            count = Math.Max((count - (negative_occourences[0] + 1 - starting_index)),
+            (negative_occourences[negative_occourences.Count - 1] - starting_index));
+
+            max_count = Math.Max(count, max_count);
+        }
+
+        return max_count;
+    }
+}
