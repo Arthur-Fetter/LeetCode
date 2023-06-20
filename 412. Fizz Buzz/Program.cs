457bbe16cba1409d2c7155d11378e8ad97b3195f
Arthur-Fetter <109872071+Arthur-Fetter@users.noreply.github.com>
2023-06-19 22:12:13 -0300

    412. Fizz Buzz
    Final answer for challenge 412. Fizz Buzz, working. 
    Beats 75.30% in runtime.
    Beats 66.87% in memory.

---
+++ 412. Fizz Buzz/Program.cs
@@ -0,0 +1,24 @@
+﻿ublic class Solution {
+    public List<string> FizzBuzz(int n) {
+        List<string> fizzbuzzes = new List<string>();
+
+        for (int i = 1; i <= n; i++){
+            if ((i % 3 == 0) && (i % 5 == 0))
+            {
+                fizzbuzzes.Add("FizzBuzz");
+            } else if (i % 3 == 0) 
+            {
+                fizzbuzzes.Add("Fizz");
+            } else if (i % 5 == 0) 
+            {
+                fizzbuzzes.Add("Buzz");
+            }
+            else
+            {
+                fizzbuzzes.Add(i.ToString());
+            }
+        }
+
+        return fizzbuzzes;
+    }
+}
